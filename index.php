70745630113b20be031f42f53dbea52a34ed74dd
Andestid <102737134+Andestid@users.noreply.github.com>
2023-03-03 22:40:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+require_once "modelo/Cliente.php";
+switch ($_SERVER['REQUEST_METHOD']) {
+    case'GET':
+        if(isset($_GET['idcliente'])) {
+            echo json_encode(Cliente::getWhere($_GET['idcliente']));
+
+        }//end if
+        else {
+             echo json_encode(Cliente::getAll());
+        }//end else
+        break;
+    
+
+    case 'POST':
+
+        $datos = json_decode(file_get_contents('php://input'));
+        if($datos != null) {
+            if(Cliente::insert($datos->nombre,$datos->apellido,$datos->telefono,$datos->fecha_nacimiento,$datos->genero)){
+                http_response_code(200);
+            }//end if
+            else{
+                http_response_code(400);
+            }//end else
+        }//end if
+        else {
+            http_response_code(405);          
+        }//end else
+        break;
+
+    case 'PUT':
+            $datos = json_decode(file_get_contents('php://input'));
+            if($datos != null) {
+                if(Cliente::update($datos->id, $datos->nombre, $datos->apellido, $datos->telefono, $datos->fecha_nacimiento,$datos->genero)){
+                    http_response_code(200);
+                }//end if
+                else{
+                    http_response_code(400);
+                }//end else
+            }//end if
+            else {
+                http_response_code(405);          
+            }//end else
+            break;
+    
+    case 'DELETE':
+        if(isset($_GET['idcliente'])){
+            if(Cliente::delete($_GET['idcliente'])){
+                http_response_code(200);
+            }//end if
+            else{
+                http_response_code(400);
+            }//end else
+        }//end if
+        else{
+            http_response_code(405);
+        }//end else
+        break;
+
+
+
+
+    default:
+    http_response_code(405);
+        # code...
+
+        break;
+ }  
+
+?>
